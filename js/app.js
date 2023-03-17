605969b6b5d0f7ea6cc9fc3b15c91254d29a2fd2
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-16 20:01:36 -0600

    new topic

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let usernames = ['Danny', 'Ben', 'Ale', 'Mula', 'Eric'];
+let user_ages = [15, 50, 45, 50, 65];
+
+let first_user_name = usernames[0];
+let last_user_name = usernames[4];
+let middle_user_age = user_ages[2];
+
+
+usernames.push('Chris');
+user_ages.push(44);
+
+let last_user_age = user_ages.pop();
